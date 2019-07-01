1f2fb5359a825165a7ef73d2a5a2abb7cbbb779a
Andre Simon <aasimon@uc.cl>
2019-07-01 01:33:36 -0400

    fixed emojis + hello tweet

--- src/App.js
+++ src/App.js
@@ -1,60 +1,26 @@
 import React from "react";
 import "./App.css";
+import { Tweet } from "react-twitter-widgets";
 
 function App() {
   return (
     <div className="App">
       <div>
         <h3>
-          I'm Andre Simon from 🇨🇱🌶 and I study CS in the Pontifical Catholic
-          University of Chile and this the about page of the Treep project.
-          Here's what I used to get inspired. Sorry about the mess. Also, feel
-          free to email me at <a href="mailto:aasimon@uc.cl">aasimon@uc.cl</a>.
-          Welcome back to the 00's...
+          I'm Andre Simon from{" "}
+          <span role="img" aria-label="Chile">
+            🇨🇱
+          </span>
+          <span role="img" aria-label="Chili">
+            🌶
+          </span>{" "}
+          and I study CS in the Pontifical Catholic University of Chile and this
+          the about page of the Treep project. Here's what I used to get
+          inspired. Sorry about the mess. Also, feel free to email me at{" "}
+          <a href="mailto:aasimon@uc.cl">aasimon@uc.cl</a>. Welcome back to the
+          00's...
         </h3>
-        <blockquote class="twitter-tweet">
-          <p lang="en" dir="ltr">
-            iPad Pro + Procreate... 🎇🎇🎇🎇{" "}
-            <a href="https://t.co/wingmIScWE">pic.twitter.com/wingmIScWE</a>
-          </p>
-          &mdash; Gasca (@gasca){" "}
-          <a href="https://twitter.com/gasca/status/1134228262517903360?ref_src=twsrc%5Etfw">
-            May 30, 2019
-          </a>
-        </blockquote>{" "}
-        <script
-          async
-          src="https://platform.twitter.com/widgets.js"
-          charset="utf-8"
-        />
-      </div>
-      <div>
-        <p>Is there going to be any progress?</p>
-        <ul>
-          <li>👀</li>
-          <li>🕊</li>
-          <li>🌱</li>
-        </ul>
-      </div>
-      <div>
-        <div>
-          If this is what you came for, here it is :-)
-          <div style={{ backgroundColor: "hotpink", textAlign: "center" }}>
-            <p>
-              rn: <a href="https://github.com/asimonv/rn-treep">📲</a>
-            </p>
-            <p>
-              node server{" "}
-              <a href="#https://github.com/asimonv/node-treep">💻</a>
-            </p>
-          </div>
-          <h1 style={{ backgroundColor: "yellow" }}>
-            PRs are very much welcomed!
-          </h1>
-          <a href="https://github.com/asimonv/rn-treep/issues/3#issue-451832492">
-            To-do's
-          </a>
-        </div>
+        <Tweet tweetId="1134228262517903360" />
       </div>
     </div>
   );
