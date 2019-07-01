8a6ef56f9370588534fd52862b0bcf150878630c
Andre Simon <aasimon@uc.cl>
2019-07-01 02:41:57 -0400

    added resume

--- src/App.js
+++ src/App.js
@@ -16,17 +16,25 @@ function App() {
           </span>{" "}
           and I study CS in the Pontifical Catholic University of Chile. Feel
           free to email me at <a href="mailto:aasimon@uc.cl">aasimon@uc.cl</a>
-          <ul style={{ marginTop: "10px" }}>
-            <li>
-              <a href="https://github.com/asimonv">
-                <img
-                  style={{ width: 20, height: 20 }}
-                  src={github}
-                  alt="github-logo"
-                />
-              </a>
-            </li>
-          </ul>
+          <div
+            style={{
+              marginTop: "10px",
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center"
+            }}
+          >
+            <a href="https://github.com/asimonv">
+              <img
+                style={{ width: 20, height: 20 }}
+                src={github}
+                alt="github-logo"
+              />
+            </a>
+            <a href="https://www.dropbox.com/s/1swbqptibfcomhy/Resume.pdf?dl=0">
+              Resume
+            </a>
+          </div>
         </h3>
         <Tweet tweetId="1134228262517903360" />
       </div>
