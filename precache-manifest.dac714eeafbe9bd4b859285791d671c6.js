894aac9d63f239d305774ee5a43e87ccb9a23e98
Andre Simon <aasimon@uc.cl>
2019-09-09 16:03:08 -0300

    Updates

---
+++ precache-manifest.dac714eeafbe9bd4b859285791d671c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7be04ba032ffdc7b0406853134188a79",
+    "url": "/react-me/index.html"
+  },
+  {
+    "revision": "68bedeb72fd4a2cb5bc7",
+    "url": "/react-me/static/css/main.ee28385c.chunk.css"
+  },
+  {
+    "revision": "0060a5a914d6973a0c4e",
+    "url": "/react-me/static/js/2.215e6fa4.chunk.js"
+  },
+  {
+    "revision": "68bedeb72fd4a2cb5bc7",
+    "url": "/react-me/static/js/main.2c4cb575.chunk.js"
+  },
+  {
+    "revision": "5de0cf6b2950e1b516e3",
+    "url": "/react-me/static/js/runtime~main.f70d4391.js"
+  }
+]);
