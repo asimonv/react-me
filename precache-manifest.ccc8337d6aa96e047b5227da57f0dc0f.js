2b076a7e24984232cf1183a1ba3e7457c6c41f46
asimonv <aasimon@uc.cl>
2020-04-10 13:16:24 -0400

    Updates

---
+++ precache-manifest.ccc8337d6aa96e047b5227da57f0dc0f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7586a01c477808879662c74590d88936",
+    "url": "/react-me/index.html"
+  },
+  {
+    "revision": "e45116c8e8818376c249",
+    "url": "/react-me/static/css/main.a184665a.chunk.css"
+  },
+  {
+    "revision": "d3cd4ab88a6a91484507",
+    "url": "/react-me/static/js/2.6058aea0.chunk.js"
+  },
+  {
+    "revision": "e45116c8e8818376c249",
+    "url": "/react-me/static/js/main.14cdf0ab.chunk.js"
+  },
+  {
+    "revision": "5de0cf6b2950e1b516e3",
+    "url": "/react-me/static/js/runtime~main.f70d4391.js"
+  },
+  {
+    "revision": "56b78509cf4402c13cd80889185ed7de",
+    "url": "/react-me/static/media/nyt-webfont.56b78509.woff"
+  },
+  {
+    "revision": "66cc4e833ac3d7e8b623882d2e4221ec",
+    "url": "/react-me/static/media/nyt-webfont.66cc4e83.woff2"
+  },
+  {
+    "revision": "373bef8dbc1a357c1006e8d42154db9f",
+    "url": "/react-me/static/media/nyt.373bef8d.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.93a240a714c279d10ab36761ef9f8935.js"
+  "/react-me/precache-manifest.ccc8337d6aa96e047b5227da57f0dc0f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-me/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
